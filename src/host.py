72dd78f527095150ad6b78361d871f3ad06bc2bf
Matthias Zeller <matthias.zeller1901@gmail.com>
2019-08-11 20:24:05 +0200

    host data analysis update
    finalization of path and computation management, missingness per chromosome, cleaning now actually occurs

--- src/host.py
+++ src/host.py
@@ -13,26 +13,29 @@ def run_shell_command(cmd_lst) :
 def run_plink(command, file, out, extension, plink2=True, verbose_err=True, force=False):
     """Returns a tuple: (stdout, stderr).
     Note: command input must have the options/values separated by a single space"""
-    #if RUN_SCRIPTS == False: return
     # Check is result already exist
+    stdout, stderr = '', ''
+    if extension[0] == '.': # avoid unexpected behaviour
+        stderr = 'ERROR: run_plink function, extension must not have a dot at beginning.'
     if force==False and os.path.exists(out+'.'+extension): 
         print("run_plink: command '{}', the file '{}' already exists (force is set to False)."
              .format(command, out+'.'+extension))
         # ABORT !
-        return ('run_plink was aborted', '')
-    # Construct the shell commands
-    lst = []
-    # Determine plink version to use
-    lst.append('plink2' if plink2 else 'plink')
-    # Input and output
-    lst.append('--bfile'); lst.append(file)
-    lst.append('--out');   lst.append(out)
-    # Must separate the options from their values (see run_shell_command)
-    commands = command.split(' ')
-    for c in commands :
-        lst.append(c)
-    # Provide values to the run_shell_command function
-    stdout, stderr = run_shell_command(lst)
+        return ('run_plink was aborted (not an error)', '')
+    if not stderr :
+        # Construct the shell commands
+        lst = []
+        # Determine plink version to use
+        lst.append('plink2' if plink2 else 'plink')
+        # Input and output
+        lst.append('--bfile'); lst.append(file)
+        lst.append('--out');   lst.append(out)
+        # Must separate the options from their values (see run_shell_command)
+        commands = command.split(' ')
+        for c in commands :
+            lst.append(c)
+        # Provide values to the run_shell_command function
+        stdout, stderr = run_shell_command(lst)
     # Display errors
     if stderr and verbose_err : print(stderr)
     # Return the outputs
