1823139478a5e2d97779399b9431b8b509202327
Matthias Zeller <matthias.zeller1901@gmail.com>
2019-08-22 13:40:33 +0200

    modif G2G computer intro, update run_gwas.sh

--- src/run_gwas.sh
+++ src/run_gwas.sh
@@ -6,9 +6,9 @@ module load gcc
 module load python
 module list
 
-echo "LAUNCHING JUPYTER NBCONVERT\n\n"
+echo "LAUNCHING JUPYTER NBCONVERT"
 
-jupyter nbconvert --execute "/home/mazeller/HBV/G2G computer.nbconvert.ipynb" --to notebook --ExecutePreprocessor.timeout=-1
+jupyter nbconvert --execute "/home/mazeller/HBV/G2G computer.ipynb" --to notebook --ExecutePreprocessor.timeout=-1
 
-echo "\n\n\nJUPYTER NBCONVERT HAS ENDED"
+echo "JUPYTER NBCONVERT HAS ENDED"
 
